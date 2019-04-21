4541140d0416244a26f1596428c354b00930eac1
Sam McHardy <hello@sammchardy.net>
2019-04-21 18:14:44 +1000

    Add fixture to reduce peer requests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,8 @@
+import json
+import os
+
 import pytest
+import requests_mock
 
 from binance_chain.http import HttpApiClient
 from binance_chain.constants import PeerType
@@ -27,26 +31,37 @@ class TestClient:
     def test_get_peers(self, httpclient):
         assert httpclient.get_peers()
 
+    def load_fixture(self, file_name):
+        this_dir = os.path.dirname(os.path.realpath(__file__))
+        with open(f'{this_dir}/fixtures/{file_name}', 'r') as f:
+            return json.load(f)
+
     @pytest.mark.parametrize("peer_type", [
         PeerType.NODE,
         PeerType.WEBSOCKET
     ])
     def test_get_peers_capability_node(self, peer_type, httpclient):
-        peers = httpclient.get_peers(peer_type=peer_type)
-        for p in peers:
-            assert peer_type in p['capabilities']
+        with requests_mock.mock() as m:
+            m.get(httpclient._create_uri('peers'), json=self.load_fixture('peers.json'))
+            peers = httpclient.get_peers(peer_type=peer_type)
+            for p in peers:
+                assert peer_type in p['capabilities']
 
     def test_get_node_peers(self, httpclient):
-        peers = httpclient.get_node_peers()
-        for p in peers:
-            assert PeerType.NODE in p['capabilities']
-        assert httpclient.get_peers()
+        with requests_mock.mock() as m:
+            m.get(httpclient._create_uri('peers'), json=self.load_fixture('peers.json'))
+            peers = httpclient.get_node_peers()
+            for p in peers:
+                assert PeerType.NODE in p['capabilities']
+            assert httpclient.get_peers()
 
     def test_get_websocket_peers(self, httpclient):
-        peers = httpclient.get_websocket_peers()
-        for p in peers:
-            assert PeerType.WEBSOCKET in p['capabilities']
-        assert httpclient.get_peers()
+        with requests_mock.mock() as m:
+            m.get(httpclient._create_uri('peers'), json=self.load_fixture('peers.json'))
+            peers = httpclient.get_websocket_peers()
+            for p in peers:
+                assert PeerType.WEBSOCKET in p['capabilities']
+            assert httpclient.get_peers()
 
     def test_get_tokens(self, httpclient):
         assert httpclient.get_tokens()
